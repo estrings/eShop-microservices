d38cf3c83242514ceceec44504859b0210ffc58b
EJEGUO EJOKPE <trustlead01@gmail.com>
2025-06-15 22:43:40 +0100

    Implemented dispatching domain events and tested ef core with docker configuration

---
+++ src/services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.Domain.Abstractions;
+
+/// <summary>
+/// Generic interface for IEntity
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface IEntity<T> : IEntity
+{
+    public T Id { get; set; }
+}
+
+
+public interface IEntity
+{
+    public DateTime? CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
